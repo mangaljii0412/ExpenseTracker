f0105192434b340dc156e7a2d59d1c63f9a0926f
Tarun Agarwal <145212493+its-tarun-2505@users.noreply.github.com>
2023-12-01 11:12:36 +0530

    Add files via upload

---
+++ homepage.php
@@ -0,0 +1,160 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="CSS/homepage.css">
+  <title>Budget Buddy</title>
+</head>
+
+<body>
+  <!-- Header -->
+  <section id="header">
+    <div class="header container">
+      <div class="nav-bar">
+        <div class="brand">
+          <a href="#hero">
+            <h1><span>B</span>udget <span>B</span>uddy</h1>
+          </a>
+        </div>
+        <div class="nav-list">
+          <div class="hamburger">
+            <div class="bar"></div>
+          </div>
+          <ul>
+            <li><a href="#hero" data-after="Home">Home</a></li>
+            <li><a href="#services" data-after="Service">Services</a></li>
+          
+            <li><a href="#about" data-after="About">About</a></li>
+            <li><a href="#contact" data-after="Contact">Contact</a></li>
+            <LI><a href="login.php" data-after="Login">-Login-</a></LI>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End Header -->
+
+
+  <!-- Hero Section  -->
+  <section id="hero">
+    <div class="hero container">
+      <div>
+        <h1>Hello, <span></span></h1>
+        <h1>Welcome To <span></span></h1>
+        <h1>Expense Tracker Web application <span></span></h1>
+        <a href="registration.php" type="button" class="cta">Sign-up</a> 
+      </div>
+    </div>
+  </section>
+  <!-- End Hero Section  -->
+
+  <!-- Service Section -->
+  <section id="services">
+    <div class="services container">
+      <div class="service-top">
+        <h1 class="section-title">Serv<span>i</span>ces</h1>
+        <p>BudgetBuddy provides a many services  to the customer and industries. Financial solutions to meet your needs whatever your money goals,there is a BudgetBuddy solution to help you reach them </p>
+      </div>
+      <div class="service-bottom">
+        <div class="service-item">
+          <div class="icon"><img src="images/setting-icon.png" /></div>
+          <h2>Personal Expenses</h2>
+          <p>Budgeting is more than paying bills and setting aside savings.it's about creating a money plan for the life you want</p>
+        </div>
+        <div class="service-item">
+          <div class="icon"><img src="images/setting-icon.png" /></div>
+          <h2>Financial Life</h2>
+          <p>Get your Complete financial picture at a glance. With BudgetBuddy application you can view your all the financial activities
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End Service Section -->
+ 
+ <!-- About Section -->
+  <section id="about">
+    <div class="about container">
+    
+        <div class="col-right">
+            <h1 class="section-title">About <span>Us</span></h1>
+            <h2>Financial Solution</h2>
+            <p>BudgetBuddy financial solution  is one among financial website. BudgetBuddy provides a financial services  to the customer. Financial solutions to meet your needs whatever your money goals,there is a BudgetBuddy solution to help you reach them. you can Contact our service center for further information and also follow our social media for update on new services </p>
+            <a href="#footer" class="cta">Follow Us</a>
+        </div>
+
+        <div class="col-left">
+            <div class="about-img">
+                <img src="images/grp-image.png" alt="img">
+                <div><h2>Budget Buddy Team </h2></div>
+            </div>
+        </div>
+        
+    </div>
+  </section>
+  <!-- End About Section -->
+
+  <!-- Contact Section -->
+  <section id="contact">
+    <div class="contact container">
+      <div>
+        <h1 class="section-title">Contact <span>info</span></h1>
+      </div>
+      <div class="contact-items">
+        <div class="contact-item">
+          <div class="icon"><img src="images/phone-icon.png" /></div>
+          <div class="contact-info">
+            <h1>Phone</h1>
+            <h2>1800 123 1234</h2>
+            <h2>1800 321 4321</h2>
+          </div>
+        </div>
+        <div class="contact-item">
+          <div class="icon"><img src="images/message-icon.png" /></div>
+          <div class="contact-info">
+            <h1>Email</h1>
+            <h2>budgetbuddy@gmail.com</h2>
+            <h2>budgetsolution@gmail.com</h2>
+          </div>
+        </div>
+        <div class="contact-item">
+          <div class="icon"><img src="images/location-icon.png" /></div>
+          <div class="contact-info">
+            <h1>Address</h1>
+            <h2>GLA University Mathura,UttarPradesh,India</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End Contact Section -->
+
+  <!-- Footer -->
+  <section id="footer">
+    <div class="footer container">
+      <div class="brand">
+        <h1><span>B</span>udget <span>B</span>uddy</h1>
+      </div>
+      <h2>Your Complete Financial Solution</h2>
+      <div class="social-icon">
+        <div class="social-item">
+          <a href="#"><img src="images/facebook-icon.png" /></a>
+        </div>
+        <div class="social-item">
+          <a href="#"><img src="images/instagram-icon.png" /></a>
+        </div>
+        <div class="social-item">
+          <a href="#"><img src="images/twitter-icon.png" /></a>
+        </div>
+      </div>
+      <p>Copyright © 2023-24 . All rights reserved</p>
+    </div>
+  </section>
+  <!-- End Footer -->
+  <script src="JavaScript/homepage.js"></script>
+</body>
+
+</html>
